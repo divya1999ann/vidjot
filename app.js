d717b36e07d1a4d03c5cd967e0b45b31a1891da6
divya1999ann <divya.kurien@gmail.com>
2019-08-01 13:14:54 +0530

    Validaton added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var mongoose = require('mongoose');
+var bodyParser=require('body-parser');
 var app = express();
 
 //map global promise
@@ -8,7 +9,7 @@ mongoose.Promise=global.Promise;
 
 //connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev',{
-
+    useNewUrlParser: true
 })
 .then(function(){
     console.log('Mongo DB Connected...');
@@ -31,6 +32,9 @@ app.use(function(req,res,next){
     next();
 });
 
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //index port
 app.get('/',function(req,res){
@@ -47,6 +51,30 @@ app.get('/about',function(req,res){
 
 app.get('/ideas/add',function(req,res){
     res.render('./ideas/add');
+  });
+
+//process
+app.post('/ideas', (req, res) => {
+    let errors = [];
+  
+    if(!req.body.title){
+      errors.push({text:'Please add a title'});
+    }
+    
+    if(!req.body.details){
+        console.log('hello');
+      errors.push({text:'Please add some details'});
+    }
+  
+    if(errors.length > 0){
+      res.render('./ideas/add', {
+        errors: errors,
+        title: req.body.title,
+        details: req.body.details
+      });
+    } else {
+      res.send('passed');
+    }
   });
 
 var port = 5000;
